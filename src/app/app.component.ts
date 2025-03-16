7808fe17ac92fc4014d0644232377b689558af89
caydende <96990579+caydende@users.noreply.github.com>
2025-03-16 07:32:07 +0200

    src code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private readonly s = inject(ToastrService)
+  clcick(){
+        this.s.success("asfa")
+  }
+  ngOnInit(): void {
+
+  }
   title = 'protofolio';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,33 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import Aura from '@primeng/themes/aura'
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideToastr, ToastrModule } from 'ngx-toastr'
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
+    provideAnimations(),
+    provideToastr(),
+    importProvidersFrom(NgxSpinnerModule , ToastrModule.forRoot({
+      timeOut: 1500,
+      progressBar: true ,
+      preventDuplicates:true,
+      resetTimeoutOnDuplicate:true,
+      extendedTimeOut:1,
+    }),),
+    ]
 };
