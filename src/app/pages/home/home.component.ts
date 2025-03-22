e0e98edb938151504bc95e1ff4570984196db01f
caydende <96990579+caydende@users.noreply.github.com>
2025-03-22 22:07:52 +0200

    hexagon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,32 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink , ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements AfterViewInit{
+  private readonly activatedRoute = inject(ActivatedRoute)
   ngAfterViewInit(): void {
+    this.routerJump()
     gsap.registerPlugin(ScrollTrigger)
   }
+
+
+routerJump():void{
+    this.activatedRoute.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    });
+}
+
 }
